bac68d3ad5516e02933e30bf3259f6569193a32d
Ekin Yang <ytdl@cn.ibm.com>
2017-08-02 09:54:36 +0800

    UIImage grayscale extension

---
+++ Detector/Common/UIImage+grey.swift
@@ -0,0 +1,32 @@
+//
+//  UIImage+grey.swift
+//  Detector
+//
+//  Created by EkinYang on 2017/8/2.
+//  Copyright © 2017年 Gregg Mojica. All rights reserved.
+//
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func grayImage() -> UIImage {
+        
+        let imageRef:CGImage = self.cgImage!
+        
+        let width:Int = imageRef.width
+        let height:Int = imageRef.height
+        
+        let colorSpace:CGColorSpace = CGColorSpaceCreateDeviceGray()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.none.rawValue)
+        let context:CGContext = CGContext(data: nil, width: width, height: height, bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+        
+        let rect:CGRect = CGRect.init(x: 0, y: 0, width: width, height: height)
+        context.draw(imageRef, in: rect)
+        let outPutImage:CGImage = context.makeImage()!
+        
+        let newImage:UIImage = UIImage.init(cgImage: outPutImage, scale: self.scale, orientation: self.imageOrientation)
+        return newImage
+    }
+
+}
